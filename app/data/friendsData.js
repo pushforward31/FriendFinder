503c08e98b4d2ed47cb83c5175c8f733255386b2
James <J.Lemons2@gmail.com>
2018-03-08 15:46:07 -0500

    update2

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,21 +1,29 @@
-var friendArray = [
-{
-name: " ",
-photo:" ",
-scores:[
-{question1: " "},
-{question2: " "},
-{question3: " "},
-{question4: " "},
-{question5: " "},
-{question6: " "},
-{question7: " "},
-{question8: " "},
-{question9: " "},
-{question10: " "}
-]
-}
-];
-
-
+var friendArray = [{
+    name: "J",
+    photo: "https://cdn3.whatculture.com/images/2017/07/f35a22604066078f-600x400.jpg",
+    scores:[
+    {question1: "5"},
+    {question2: "3"},
+    {question3: "3"},
+    {question4: "4"},
+    {question5: "2"},
+    {question6: "3"},
+    {question7: "1"},
+    {question8: "2"},
+    {question9: "4"},
+    {question10: "4"}
+    ]
+    // score: [
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4",
+    //     "4"
+    // ]
+}]
 module.exports = friendArray;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,29 +1,61 @@
 var friendData = require("../data/friendsData");
 
-module.exports = function(app){
+module.exports = function(app) {
 
 
-	app.get("/api/friends", function (req, res) {
-		res.json.stringify(friendData);
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
 
-	});
-
-
+    });
 
 
 
-	app.post("/api/friends", function(req, res){
-
-		friendData.push(req.body);
-		res.json(true);
-	});
 
 
-	app.post("/api/clear", function(){
+    app.post("/api/friends", function(req, res) {
 
-		friendData =[];
+        //grabs the new friend's scores to compare with friends in friendData array
+        var newFriendScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+
+        //runs through all current friends in list
+        for (var i = 0; i < friendData.length; i++) {
+            var scoresDiff = 0;
+            //run through scores to compare friends
+            for (var j = 0; j < newFriendScores.length; j++) {
+                scoresDiff += (Math.abs(parseInt(friendData[i].scores[j]) - parseInt(newFriendScores[j])));
+            }
+
+            //push results into scoresArray
+            scoresArray.push(scoresDiff);
+        }
+
+        //after all friends are compared, find best match
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        //return bestMatch data
+        var future = friendData[bestMatch];
+        res.json(future);
+
+        //pushes new submission into the friendsList array
+        friendData.push(req.body);
+    });
+
+    //res.json(true);
 
 
-		console.log(friendData);
-	});
+
+    app.post("/api/clear", function() {
+
+        friendData = [];
+
+
+        console.log(friendData);
+    });
 }
